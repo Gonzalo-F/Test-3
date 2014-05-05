abfa0544251c040ebc1d722de5423dac1b9de470
emiiSierra <sierraemii@gmail.com>
2014-05-05 18:17:59 -0300

    test varios jugadores

--- TP anual/src/tests/TestsInscripcionVariosJugadores.xtend
+++ TP anual/src/tests/TestsInscripcionVariosJugadores.xtend
@@ -5,30 +5,24 @@ import opfv2.Jugador
 import opfv2.Partido
 import org.junit.Before
 import org.junit.Test
-
 import static org.junit.Assert.*
 import opfv2.InscripEstandar
 
 class TestsInscripcionVariosJugadores {
 
-	Partido partido
+	var Partido partido = new Partido(2040, 20140413, "El tinglado")
 
 	@Before
 	def testInicio() {
-		var Partido partido = new Partido(2040, 20140413, "El tinglado")
 		for (i : 1 .. 10) {
-
 			var Jugador jugador = new Jugador(i, "Player" + i)
 			jugador.incribirseEn(partido, new InscripEstandar)
-
 		}
 	}
 
 	@Test
 	def testInscribirJugadorMasde10enEstandarRechaza() {
-
 		var Jugador jugador11 = new Jugador(15, "Player11")
-
 		try {
 			jugador11.incribirseEn(partido, new InscripEstandar)
 		} catch (InscripcionRechazadaException e) {
--- TP anual/xtend-gen/tests/TestsInscripcionVariosJugadores.java
+++ TP anual/xtend-gen/tests/TestsInscripcionVariosJugadores.java
@@ -1,32 +1,44 @@
 package tests;
 
+import excepciones.InscripcionRechazadaException;
 import opfv2.InscripEstandar;
 import opfv2.Jugador;
 import opfv2.Partido;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.IntegerRange;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 @SuppressWarnings("all")
 public class TestsInscripcionVariosJugadores {
-  private Partido partido;
+  private Partido partido = new Partido(2040, 20140413, "El tinglado");
   
   @Before
   public void testInicio() {
-    Partido partido = new Partido(2040, 20140413, "El tinglado");
     IntegerRange _upTo = new IntegerRange(1, 10);
     for (final Integer i : _upTo) {
       {
         Jugador jugador = new Jugador((i).intValue(), ("Player" + i));
         InscripEstandar _inscripEstandar = new InscripEstandar();
-        jugador.incribirseEn(partido, _inscripEstandar);
+        jugador.incribirseEn(this.partido, _inscripEstandar);
       }
     }
   }
   
   @Test
   public void testInscribirJugadorMasde10enEstandarRechaza() {
-    throw new Error("Unresolved compilation problems:"
-      + "\nInscripcionRechazadaException cannot be resolved to a type.");
+    Jugador jugador11 = new Jugador(15, "Player11");
+    try {
+      InscripEstandar _inscripEstandar = new InscripEstandar();
+      jugador11.incribirseEn(this.partido, _inscripEstandar);
+    } catch (final Throwable _t) {
+      if (_t instanceof InscripcionRechazadaException) {
+        final InscripcionRechazadaException e = (InscripcionRechazadaException)_t;
+        Assert.fail();
+      } else {
+        throw Exceptions.sneakyThrow(_t);
+      }
+    }
   }
 }
